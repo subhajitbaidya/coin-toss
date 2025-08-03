879cff92f1c556afc204d854ecaa6cdcbf04488d
Subhajit Baidya <subhajitbaidy43@gmail.com>
2025-08-03 21:54:22 +0530

    fixed

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/coin-toss/', // ✅ Set to your repo name
-  plugins: [react()],
-})
+  plugins: [react(), tailwindcss()],
+});
